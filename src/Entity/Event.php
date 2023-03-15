62e44d1e5bfefcb43fa3b38059e422266e527618
Romain Trochu <romain.trochu@gmail.com>
2023-03-15 21:58:03 +0100

    create entity-event, migration

---
+++ src/Entity/Event.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $price = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $startEvent = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $endEvent = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $cover = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getStartEvent(): ?\DateTimeImmutable
+    {
+        return $this->startEvent;
+    }
+
+    public function setStartEvent(\DateTimeImmutable $startEvent): self
+    {
+        $this->startEvent = $startEvent;
+
+        return $this;
+    }
+
+    public function getEndEvent(): ?\DateTimeImmutable
+    {
+        return $this->endEvent;
+    }
+
+    public function setEndEvent(\DateTimeImmutable $endEvent): self
+    {
+        $this->endEvent = $endEvent;
+
+        return $this;
+    }
+
+    public function getCover(): ?string
+    {
+        return $this->cover;
+    }
+
+    public function setCover(?string $cover): self
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+}
