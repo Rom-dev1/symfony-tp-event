a2a5246426efe3ee3300e1d7cacf1206d3344733
Romain Trochu <romain.trochu@gmail.com>
2023-03-15 22:38:43 +0100

    create fixture to insert table event

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Event;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Validator\Constraints\Date;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    {   
 
+        for($i = 0; $i<5; $i++){
+            $date = new DateTimeImmutable();
+            $event = new Event();
+            $event->setName("Test");
+            $event->setPrice("4000");
+            $event->setCreatedAt($date);
+            $event->setStartEvent($date);
+            $event->setEndEvent($date);
+            $manager->persist($event);
+        }
         $manager->flush();
     }
 }
