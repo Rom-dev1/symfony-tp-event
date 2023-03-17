ab937f91a944ebf72d814d5ea03ef22a1b9ce720
Romain Trochu <romain.trochu@gmail.com>
2023-03-17 11:02:02 +0100

    implement SearchData to HomeCrontroller, get request

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
+use App\SearchData;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(Request $request)
+    {   
+        $searchBar = new SearchData($request);
+        dump($searchBar);
+        $form = $this->createForm(SearchType::class, $searchBar);
+        dump($form);
+        $form->handleRequest($request);
+        dump($form);
+        if($form->isSubmitted() && $form->isValid()){
+            dump($searchBar);
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'form' => $form
         ]);
     }
 }
