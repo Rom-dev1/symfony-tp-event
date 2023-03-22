476520675669d890fd9e25571284efd8fa9e1667
Romain Trochu <romain.trochu@gmail.com>
2023-03-22 22:51:27 +0100

    make user, fix hash

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,22 @@
 namespace App\DataFixtures;
 
 use App\Entity\Event;
+use App\Entity\User;
 use DateTime;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
     public function load(ObjectManager $manager): void
     {   
         $faker = Factory::create('fr_FR');
@@ -29,6 +37,14 @@ class AppFixtures extends Fixture
             $event->setEndEvent($endDate);
             $manager->persist($event);
         }
+
+        $user = new User();
+        $user->setEmail('test@mail.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->hasher->hashPassword($user, 'password'));
+        $user->setName('test');
+        $manager->persist($user);
+
         $manager->flush();
     }
 }
